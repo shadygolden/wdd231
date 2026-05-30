58c3b4b672f742bd4fb11362da6310c88bbc2d71
shadygolden <shadrackchineduuzoka@gmail.com>
2026-05-30 17:06:13 +0100

    add edited join html

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,17 @@
-const timestamp = document.getElementById("timestamp");
 
-timestamp.value = new Date().toISOString();
+const modalLinks = document.querySelectorAll(".modal-link");
+
+modalLinks.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const modalId = link.dataset.modal;
+        document.getElementById(modalId).showModal();
+    });
+});
+
+document.querySelectorAll(".close-modal").forEach(button => {
+    button.addEventListener("click", () => {
+        button.closest("dialog").close();
+    });
+});
